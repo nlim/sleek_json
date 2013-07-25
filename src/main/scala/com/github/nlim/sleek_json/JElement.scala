f474aab352b69c9af426bbf657291cd8b9217250
Nathaniel Lim <nathaniel.j.lim@gmail.com>
2013-07-25 13:51:05 -0400

    methods

--- src/main/scala/com/github/nlim/sleek_json/JElement.scala
+++ src/main/scala/com/github/nlim/sleek_json/JElement.scala
@@ -16,10 +16,21 @@ case object JNull extends JElement
 
 case class JArray(value: Vector[JElement]) extends JElement {
   def get(i: Int) = value(i)
+
+  def ++(other: JArray): JArray = JArray(value ++ other.value)
+
+  def :+(elem: JElement): JArray = JArray(value :+ elem)
+  def +:(elem: JElement): JArray = JArray(elem +: value)
 }
 
 case class JObject(value: Map[String, JElement]) extends JElement {
   def get(k: String) = value(k)
+
+  def ++(other: JObject): JObject = JObject(value ++ other.value)
+
+  def +(t: (String, JElement)): JObject = JObject(value + t)
+
+  def -(t: String): JObject = JObject(value - t)
 }
 
 case class JInteger(value: Integer) extends JElement
